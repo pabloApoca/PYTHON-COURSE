26e580d5708fbdf06af39b88285de2be91b8f650
Pablo Lopez <pablo_mtv@hotmail.com>
2020-06-09 16:02:23 -0300

    Tipos de datos

---
+++ datetype.py
@@ -0,0 +1,26 @@
+# String
+print("Hello World")
+print("""Hello World""")
+print('Hello World')
+print('''Hello World''')
+print(type("Holaaaa"))
+
+# Integer
+print(30)
+
+# Float
+print(30.5)
+
+# Boolean
+True
+False
+
+# List
+[10, 20, 30]
+["Hello", "Bye", "GoodBye"]
+["Hello", 100, False, 30.7]
+
+# Tuples - Los datos aca no pueden cambiar (Tienen que ser todos iguales)
+(10, 20, 30, 40)
+
+# Dictionaries - Permiten agrupar datos que pertenescan a una misma entidad
---
+++ helloworld.py
@@ -0,0 +1,2 @@
+#Este codigo imprime un msj en pantalla
+print("Hello World")
